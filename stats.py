6205255d6e1af3b46747ecdd74358c45c575d005
Jakarin Chujan <jojogamer67@gmail.com>
2021-10-04 15:18:40 +0700

    Fix a stats.py

--- stats.py
+++ stats.py
@@ -1,3 +1,6 @@
+import math
+
+
 def variance(data):
     """Returns the population variance of a list (array) of numbers in data.
     
@@ -30,11 +33,11 @@ def variance(data):
     # some deliberately misformatted code. Use flake8 to fix.
     n = len(data)
     if n == 0:
-        raise TypeError()
+        raise ValueError()
     average = sum(data) / n
     return sum([(x - average) ** 2 for x in data]) / n
 
 
 def stdev(data):
     """The population standard deviation of a list of data values."""
-    return sqrt(variance(data))
+    return math.sqrt(variance(data))
